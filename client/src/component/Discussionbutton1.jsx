5b93d7b104e329b2efadb25b4316f4954b8700b0
Tzivya Fireman <tz0534199716@gmail.com>
2025-05-18 14:33:18 +0300

    remove comments in Discussionbutton1.jsx

--- client/src/component/Discussionbutton1.jsx
+++ client/src/component/Discussionbutton1.jsx
@@ -1,34 +1,3 @@
-// import { Button } from 'primereact/button';
-// import { useState } from 'react';
-// import ShowDiscussion from './ShowDiscussion'
-// import {useUpdateDiscussionReadMutation} from '../app/discussionApiSlice'
-// import { useNavigate } from 'react-router-dom';
-// const Disscussionbutton = (props) => {
-//     // const [update,{ data, isError, isSuccess }] = useUpdateDiscussionReadMutation()
-//     const [show, setShow] = useState(false)
-//     // const [read, setRead] = useState(props.discussion.read)
-//     // const c = read ? 'unset' : 'large'
-//     // const w=read?'150px':'180px'
-//     const navigate=useNavigate()
-//     const onClickButton = () => {
-//         setShow(!show)
-//         const obj={id:props.discussion._id,read:"true"}
-        
-//         // setRead(true)
-//         // update(obj)
-//         navigate(`/ShowDiscussion`,{state:{discussion:props.discussion}})
-//     }
-
-//     return (
-//         <>
-//            <Button label={props.discussion.discussionName} onClick={() =>onClickButton()} style={{width:'150px',justifyContent:'center'}}/>
-//             <br /><br />
-//             {/* {show && <ShowDiscussion discussion={props.discussion} refetch={props.refetch} />} */}
-//         </>
-//     )
-// }
-// export default Disscussionbutton
-
 import { Button } from 'primereact/button';
 import { useEffect, useState } from 'react';
 import ShowDiscussion from './ShowDiscussion'
@@ -43,25 +12,16 @@ const Disscussionbutton = (props) => {
     const { discussion,name } = location.state||{}
     const [show, setShow] = useState(false)
    const date=props?.discussion?.updatedAt?.slice(0,10) 
-   
-//    props?.discussion?.userId?.name}
-//    props?.discussion?.discussionName
-    // const [read, setRead] = useState(props.discussion.read)
-    // const c = read ? 'unset' : 'large'
-    // const w=read?'150px':'180px'
     const navigate=useNavigate()
     const onClickButton = () => {
         setShow(!show)
         const obj={id:props.discussion._id,read:"true"}
         
-        // setRead(true)
         update(obj)
-        // navigate(`/ShowDiscussion`,{state:{discussion:props.discussion}})
         navigate(`/ShowDiscussion`, { state: { discussion: props?.discussion?props.discussion:discussion, name: props?.name?props.name:name  } })
 
     }
     
-    // ||discussion.updatedAt.slice(0,10)
     return (
         <div style={{ width: '100%', alignItems: 'center' }}>
             <br />
